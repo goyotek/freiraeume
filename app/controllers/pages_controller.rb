8e4d7f4f95b9730d12d956181cfb0069eaf8115c
Greg Ohr <goyotek@gmail.com>
2013-04-17 12:01:56 +0200

    Initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+	def about
+	end
+
+	def sommerakademie
+	end
+
+	def cirkusschule		
+	end
+
+	def pachamama		
+	end
+	
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = '509d8197ca2bd3ea715a5ca248bc394623c27853d73d79bcff48fe3417e9211093bb4717578f9c983e5249d3a025685af83664e9f0cf5c0f373a2f62fb845d87'
